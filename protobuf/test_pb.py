a1fede3e4d01bd40a813ee36a91d08c37523a76e
zhaoyafei <zhaoyafei@baidu.com>
2020-04-08 18:34:56 +0800

    update protobuf/*

--- protobuf/test_pb.py
+++ protobuf/test_pb.py
@@ -1,4 +1,8 @@
 import addressbook_pb2
+from google.protobuf.json_format import MessageToJson
+
+
+# init and set an object
 person = addressbook_pb2.Person()
 person.id = 1234
 person.name = "John Doe"
@@ -11,3 +15,19 @@ phone.type = addressbook_pb2.Person.HOME
 #person.no_such_field = 1  # raises AttributeError
 ## test wrong type
 #person.id = "1234"        # raises TypeError
+
+print '\n===> proto data'
+print '-> type: ', type(person)
+print '-> data: ', person
+
+print '\n===> proto serialized string'
+serialized_str = person.SerializeToString()
+print '-> type: ', type(serialized_str)
+print '-> string: ', serialized_str 
+print '-> string length: ', len(serialized_str)
+
+print '\n===> proto MessageToJson(person) string'
+json_str = MessageToJson(person)
+print '-> type: ', type(json_str)
+print '-> string: ', json_str 
+print '-> string length: ', len(json_str)
